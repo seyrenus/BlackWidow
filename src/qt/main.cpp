ceca07b07aa9ba8ec432facd3572a5281fc54034
JackeyLea <1768478912@qq.com>
2020-01-26 18:11:07 +0800

    重置项目

--- src/qt/main.cpp
+++ src/qt/main.cpp
@@ -1,13 +1,11 @@
 #include "mainwindow.h"
+
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.setWindowTitle(QObject::tr("BlackWidow"));
-    w.setWindowIcon(QIcon(":resources/icons/spider.svg"));
     w.show();
-
     return a.exec();
 }
